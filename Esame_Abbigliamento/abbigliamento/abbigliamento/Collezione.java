588454278817a1dfcd5dfc1e51a7bc8247d2e6e5
Lorenzo <s285360@studenti.polito.it>
2025-01-23 10:29:42 +0100

    finito r4

--- Esame_Abbigliamento/abbigliamento/abbigliamento/Collezione.java
+++ Esame_Abbigliamento/abbigliamento/abbigliamento/Collezione.java
@@ -1,27 +1,40 @@
 package abbigliamento;
 
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Collezione {
 
+	private List<Capo> collezione = new LinkedList<>();
+	
+	
 	public void add(Capo capo) {
-		// TODO Auto-generated method stub
+		collezione.add(capo);
 		
 	}
 
 	public Collection trova(Colore colore) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Capo> cercaColore = collezione.stream()
+					.filter(c -> c.getColore().getNome().equals(colore.getNome()))
+					.collect(Collectors.toList());
+		return cercaColore;
 	}
 
 	public Collection trova(Materiale materiale) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Capo> cercaMateriale = collezione.stream()
+					.filter(c -> c.getMateriale().getNome().equals(materiale.getNome()))
+					.collect(Collectors.toList());
+		return cercaMateriale;
 	}
 
 	public Collection trova(Modello modello) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Capo> cercaModello = collezione.stream()
+					.filter(c -> c.getModello().getNome().equals(modello.getNome()))
+					.collect(Collectors.toList());
+		return cercaModello;
 	}
 
 }
